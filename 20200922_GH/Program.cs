44e5f880d738501f9a53ca1bebc676bea79836a0
Roland Zajkó <zajkorolandvlszki@outlook.hu>
2020-09-22 10:18:55 +0200

    :D

---
+++ 20200922_GH/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20200922_GH
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Zajkó Roland");
+            Console.ReadKey(true);
+        }
+    }
+}
